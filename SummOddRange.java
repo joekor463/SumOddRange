95a2c331dd18058e70bac1188b4a724e1b41528d
Evgeny Korobenkov <joekor463@gmail.com>
2020-05-08 23:37:24 +0800

    Summ odd range

---
+++ SummOddRange.java
@@ -0,0 +1,24 @@
+public class SummOddRange {
+    public static void main(String[] args) {
+        System.out.println(sumOdd ( 100, 1000 ));
+    }
+
+    public static boolean isOdd (int number){
+        if (number < 0) {return false;}
+        if ((number%2) == 0) {return false;}
+        else return true;
+    }
+
+    public static int sumOdd(int start, int end){
+        int sum = 0;
+        if ((start <= 0) | (end <= 0)) return -1;
+        if ((end < start)) return -1;
+        for (int i = start; i <= end; ++i) {
+            if (isOdd ( i ) == true) {
+                sum = sum + i;
+                }
+            }
+        return sum;
+    }
+}
+
